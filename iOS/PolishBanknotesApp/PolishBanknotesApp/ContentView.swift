8c4688c2e069dfec0532ef8a5084651b0f4b93c9
Buzeqq <miloszchoj@gmail.com>
2022-11-12 20:47:32 +0100

    Preview layer with device orientation change handling

--- iOS/PolishBanknotesApp/PolishBanknotesApp/ContentView.swift
+++ iOS/PolishBanknotesApp/PolishBanknotesApp/ContentView.swift
@@ -8,10 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject private var model = FrameHandler()
-    
     var body: some View {
-        FrameView(image: model.frame)
+        HostedViewController()
             .ignoresSafeArea()
     }
 }
---
+++ iOS/PolishBanknotesApp/PolishBanknotesApp/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  PolishBanknotesApp
+//
+//  Created by Miłosz Chojnacki on 12/11/2022.
+//
+
+import UIKit
+import AVFoundation
+import SwiftUI
+
+class ViewController: UIViewController {
+    private var permissionGranted = false
+    
+    private let captureSession = AVCaptureSession()
+    private let sessionQueue = DispatchQueue(label: "sessionQueue")
+    
+    private var previewLayer = AVCaptureVideoPreviewLayer()
+    var screenRect: CGRect! = nil
+    
+    override func viewDidLoad() {
+        checkPermission()
+        
+        sessionQueue.async { [unowned self] in
+            guard permissionGranted else { return }
+            self.setupCaptureSession()
+            self.captureSession.startRunning()
+        }
+    }
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        screenRect = UIScreen.main.bounds
+        self.previewLayer.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
+        
+        switch UIDevice.current.orientation {
+        case UIDeviceOrientation.portraitUpsideDown:
+            self.previewLayer.connection?.videoOrientation = .portraitUpsideDown
+            
+        case UIDeviceOrientation.landscapeLeft:
+            self.previewLayer.connection?.videoOrientation = .landscapeRight
+            
+        case UIDeviceOrientation.landscapeRight:
+            self.previewLayer.connection?.videoOrientation = .landscapeLeft
+            
+        case UIDeviceOrientation.portrait:
+            self.previewLayer.connection?.videoOrientation = .portrait
+        
+        default:
+            break
+        }
+    }
+    
+    private func checkPermission() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            permissionGranted = true
+            
+        case .notDetermined:
+            requestPermission()
+            
+        default:
+            permissionGranted = false
+            
+        }
+    }
+    
+    private func requestPermission() {
+        sessionQueue.suspend()
+        AVCaptureDevice.requestAccess(for: .video) { [unowned self] granted in
+            self.permissionGranted = granted
+            self.sessionQueue.resume()
+        }
+    }
+    
+    private func setupCaptureSession() {
+        guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else { return }
+        guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice) else { return }
+        guard captureSession.canAddInput(videoDeviceInput) else { return }
+        captureSession.addInput(videoDeviceInput)
+        
+        screenRect = UIScreen.main.bounds
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        
+        previewLayer.connection?.videoOrientation = .portrait
+        
+        DispatchQueue.main.async { [weak self] in
+            self!.view.layer.addSublayer(self!.previewLayer)
+        }
+    }
+}
+
+struct HostedViewController: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        return ViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        
+    }
+}
