a25bc77c1fc78c5c92bf62e3108fe8d86a6d2183
Kuba <106245989+Corax0x01@users.noreply.github.com>
2023-04-13 21:52:46 +0200

    88 change method of calculating brightness (#96)

--- Android/PolishBanknotesApp/app/src/client/java/pg/eti/project/polishbanknotes/sensors/TorchManager.kt
+++ Android/PolishBanknotesApp/app/src/client/java/pg/eti/project/polishbanknotes/sensors/TorchManager.kt
@@ -2,13 +2,9 @@ package pg.eti.project.polishbanknotes.sensors
 
 import android.content.Context
 import android.graphics.Bitmap
-import android.util.Log
-import android.view.accessibility.AccessibilityManager
 import androidx.camera.core.Camera
-import androidx.core.graphics.get
-import androidx.core.graphics.luminance
+import androidx.core.graphics.*
 import androidx.preference.PreferenceManager
-import pg.eti.project.polishbanknotes.settings_management.DEFAULT_PREFERENCES_FLAG
 
 const val MILLIS_TO_CHECK_TORCH = 2000L
 
@@ -36,14 +32,17 @@ class TorchManager {
 
     fun calculateBrightness(image: Bitmap, camera: Camera?, inferenceMillisCounter: Long){
         if (inferenceMillisCounter >= MILLIS_TO_CHECK_TORCH && isActive) {
+            var pixelCounter = 0
             var brightness = 0.0
+
             for (h in 0 until image.height) {
-                for (w in 0 until image.width) {
+                for (w in 0 until image.width step 10) {
                     brightness += image[w, h].luminance
+                    pixelCounter += 1
                 }
             }
 
-            val imageBrightness = brightness / (image.width * image.height)
+            val imageBrightness = brightness / pixelCounter
 
             if (imageBrightness < LUMINANCE_THRESHOLD) {
                 enableTorch(camera)
