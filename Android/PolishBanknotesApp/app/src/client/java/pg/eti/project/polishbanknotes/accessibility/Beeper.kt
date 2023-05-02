ec6099d5be6114e42c1bd60483284ed1e7ea800c
jankejc <jkornacki@o2.pl>
2023-05-02 13:08:42 +0200

    Update Beeper.kt

--- Android/PolishBanknotesApp/app/src/client/java/pg/eti/project/polishbanknotes/accessibility/Beeper.kt
+++ Android/PolishBanknotesApp/app/src/client/java/pg/eti/project/polishbanknotes/accessibility/Beeper.kt
@@ -4,20 +4,21 @@ import android.content.Context
 import android.content.Context.AUDIO_SERVICE
 import android.media.AudioManager
 import android.media.ToneGenerator
-import android.media.ToneGenerator.MAX_VOLUME
+import android.util.Log
 import androidx.preference.PreferenceManager
 
+const val VOLUME_MULTIPLICITY = 200
 
 class Beeper(var context: Context) {
     private var am: AudioManager?
-    private var volumeLevel: Int
+//    private var volumeLevel: Int
     private var toneGen: ToneGenerator
     private var isActive = true
 
     init {
         am = context.getSystemService(AUDIO_SERVICE) as AudioManager?
-        volumeLevel = am?.getStreamVolume(AudioManager.STREAM_MUSIC)!!
-        toneGen = ToneGenerator(AudioManager.STREAM_MUSIC, MAX_VOLUME)
+//        volumeLevel = am?.getStreamVolume(AudioManager.STREAM_MUSIC)!!
+        toneGen = ToneGenerator(AudioManager.STREAM_MUSIC, Int.MAX_VALUE)
     }
 
     fun checkIfEnable(context: Context) {
@@ -32,10 +33,12 @@ class Beeper(var context: Context) {
     }
 
     fun beep() {
-        toneGen.startTone(ToneGenerator.TONE_CDMA_PIP, 150)
+        toneGen.startTone(ToneGenerator.TONE_CDMA_EMERGENCY_RINGBACK, 125)
+        // TONE_CDMA_EMERGENCY_RINGBACK - slightly lower
+        // TONE_CDMA_SOFT_ERROR_LITE - slightly higher (diff. init)
     }
 
     fun doubleBeep() {
-        toneGen.startTone(ToneGenerator.TONE_CDMA_PIP, 350)
+        toneGen.startTone(ToneGenerator.TONE_CDMA_EMERGENCY_RINGBACK, 260)
     }
 }
